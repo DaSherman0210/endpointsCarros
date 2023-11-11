dcc06f6c65a11f257e2e633c75cd849c773d8ddb
DaSherman0210 <tgerman181@gmail.com>
2023-11-11 08:14:30 -0500

    feat: :sparkles: Initializing the swagger doc
    Adding the swagger documentation to this project

---
+++ swaggerConfig.js
@@ -0,0 +1,16 @@
+import swaggerJSDoc from "swagger-jsdoc";
+
+const options = {
+    swaggerDefinition:{
+        openapi: '3.0.0',
+        info:{
+            title: "AUTOMOBILES ENDPOINTS",
+            version: "3.0.0",
+            description: "Here is the documentation to this endpoints of this project."
+        }
+    },
+    apis:[
+        './src/routes/auth.routes.js',
+        './src/routes/mn.routes.js'
+    ]
+}
