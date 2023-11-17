96b2f901ab0c537fdc744d3b493e9740b988a763
DaSherman0210 <tgerman181@gmail.com>
2023-11-17 18:05:59 -0500

    feat: :truck: Added a new route
    Added the documentation route to the index file

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ import dotenv from "dotenv";
 import express from "express";
 import router from "./src/routes/main.routes.js"
 import routerAuth from "./src/routes/auth.routes.js";
+import swaggerUI from "swagger-ui-express";
+import swaggerSpec from "./swaggerConfig.js";
 
 const app = express();
 
@@ -11,6 +13,7 @@ app.use(express.json());
 
 app.use("/", router);
 app.use("/auth", routerAuth);
+app.use("/documentation", swaggerUI.serve, swaggerUI.setup(swaggerSpec));
 
 const port = process.env.PORT8327;
 
